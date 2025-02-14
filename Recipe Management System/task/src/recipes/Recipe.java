df954811d057035a83a8b2abfef68cf93d77e8fa
kwilkes <kimberly.wilkes@nysed.gov>
2025-02-14 16:22:35 -0500

    Step 2 version of Recipe

---
+++ Recipe Management System/task/src/recipes/Recipe.java
@@ -0,0 +1,42 @@
+package recipes;
+import java.util.List;
+
+public class Recipe {
+    private String name;
+    private String description;
+    private List<String> ingredients;
+    private List<String> directions;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<String> getDirections() {
+        return directions;
+    }
+
+    public void setDirections(List<String> directions) {
+        this.directions = directions;
+    }
+}
